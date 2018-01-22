890b6021891839c642267674e06b4cb945cfcb88
Combeubeu <anthony.comberousse@etu.univ-lyon1.fr>
2018-01-22 14:50:47 +0100

    excel

---
+++ src/Services/Excel.php
@@ -0,0 +1,26 @@
+<?php 
+function recuperer(){
+echo "<table>"; 
+$fichier = "../fiche listing pour BD.csv"; 
+$fic = fopen($fichier, 'rb'); 
+$nb_lignes=0;   //Init du nombre de lignes
+$nb_lignes_max=3;  //On parametre ici le nombre max de lignes a afficher
+$nb_colonnes_max=25;  //On parametre ici le nombre max de colonnes a afficher
+for ($ligne = fgetcsv($fic, 1024, ";"); !feof($fic); $ligne = fgetcsv($fic, 1024, ";")) { 
+  if($nb_lignes<$nb_lignes_max){
+    echo "<tr>"; 
+    $j = sizeof($ligne); 
+    if($j>$nb_colonnes_max){$j=$nb_colonnes_max;} // On limite à 2 colonnes
+    for ($i = 0; $i < $j; $i++) { 
+      echo "<td>".$ligne[$i]."</td>"; 
+    } 
+    echo "</tr>"; 
+  }
+  $nb_lignes++;
+ 
+} 
+echo "</table>\n"; 
+}
+recuperer();
+
+?>
---
+++ src/Services/ExcelPhp.php
@@ -0,0 +1,26 @@
+<?php 
+function recuperer(){
+echo "<table>"; 
+$fichier = "../fichier info venant de FFJ.csv"; 
+$fic = fopen($fichier, 'rb'); 
+$nb_lignes=0;   //Init du nombre de lignes
+$nb_lignes_max=27;  //On parametre ici le nombre max de lignes a afficher
+$nb_colonnes_max=40;  //On parametre ici le nombre max de colonnes a afficher
+for ($ligne = fgetcsv($fic, 1024, ";"); !feof($fic); $ligne = fgetcsv($fic, 1024, ";")) { 
+  if($nb_lignes<$nb_lignes_max){
+    echo "<tr>"; 
+    $j = sizeof($ligne); 
+    if($j>$nb_colonnes_max){$j=$nb_colonnes_max;} // On limite à 2 colonnes
+    for ($i = 0; $i < $j; $i++) { 
+      echo "<td>".$ligne[$i]."</td>"; 
+    } 
+    echo "</tr>"; 
+  }
+  $nb_lignes++;
+ 
+} 
+echo "</table>\n"; 
+}
+recuperer();
+
+?>
