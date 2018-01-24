f9f71f6bb62b170e9d91ac46362b9bdfca0d0092
Combeubeu <anthony.comberousse@etu.univ-lyon1.fr>
2018-01-24 11:46:58 +0100

    set

--- src/Models/AdherentAdministratif.php
+++ src/Models/AdherentAdministratif.php
@@ -46,67 +46,169 @@ class AdherentAdministratif
 		return $this->no_adherent;
 	}
 
+	public function setNoAdherent($_no_adherent){
+		if (!is_null($_no_adherent)) {
+			$this->no_adherent = $_no_adherent;
+		}
+	}
+
 	public function getNom(){
 		return $this->nom;
+	}
+
+	public function setNom($_nom){
+		if (!is_null($_nom)) {
+			$this->nom = $_nom;
+		}
 	}
 
 	public function getPrenom(){
 		return $this->prenom;
 	}
 
+	public function setPrenom($_prenom){
+		if (!is_null($_prenom)) {
+			$this->prenom = $_prenom;
+		}
+	}
+
 	public function getSexe(){
 		return $this->sexe;
+	}
+
+	public function setSexe($_sexe){
+		if (!is_null($_sexe)) {
+			$this->sexe = $_sexe;
+		}
 	}
 
 	public function getDateN(){
 		return $this->date_n;
 	}
 
+	public function setDateN($_date_n){
+		if (!is_null($_date_n)) {
+			$this->date_n = $_date_n;
+		}
+	}
+
 	public function getLieuN(){
 		return $this->lieu_n;
+	}
+
+	public function setLieuN($_lieu_n){
+		if (!is_null($_lieu_n)) {
+			$this->lieu_n = $_lieu_n;
+		}
 	}
 
 	public function getNationalite(){
 		return $this->nationalite;
 	}
 
+	public function setNationalite($_nationalite){
+		if (!is_null($_nationalite)) {
+			$this->nationalite = $_nationalite;
+		}
+	}
+
 	public function getNoSecuSociale(){
 		return $this->no_secu_sociale;
+	}
+
+	public function setNoSecuSociale($_no_secu_sociale){
+		if (!is_null($_no_secu_sociale)) {
+			$this->no_secu_sociale = $_no_secu_sociale;
+		}
 	}
 
 	public function getAdresse(){
 		return $this->adresse;
 	}
 
+	public function setAdresse($_adresse){
+		if (!is_null($_adresse)) {
+			$this->adresse = $_adresse;
+		}
+	}
+
 	public function getCodePostal(){
 		return $this->code_postal;
+	}
+
+	public function setCodePostal($_code_postal){
+		if (!is_null($_code_postal)) {
+			$this->code_postal = $_code_postal;
+		}
 	}
 
 	public function getVille(){
 		return $this->ville;
 	}
 
+	public function setVille($_ville){
+		if (!is_null($_ville)) {
+			$this->ville = $_ville;
+		}
+	}
+
 	public function getTelF(){
 		return $this->tel_f;
+	}
+
+	public function setTelF($_tel_f){
+		if (!is_null($_tel_f)) {
+			$this->tel_f = $_tel_f;
+		}
 	}
 
 	public function getTelM1(){
 		return $this->tel_m_1;
 	}
 
+	public function setTelM1($_tel_m_1){
+		if (!is_null($_tel_m_1)) {
+			$this->tel_m_1 = $_tel_m_1;
+		}
+	}
+
 	public function getTelM2(){
 		return $this->tel_m_2;
 	}
+
+	public function setTelM2($_tel_m_2){
+		if (!is_null($_tel_m_2)) {
+			$this->tel_m_2 = $_tel_m_2;
+		}
+	}
 	public function getEmail(){
 		return $this->email;
+	}
+
+	public function setEmail($_email){
+		if (!is_null($_email)) {
+			$this->email = $_email;
+		}
 	}
 
 	public function getNewsletter(){
 		return $this->newsletter;
 	}
 
+	public function setNewsletter($_newsletter){
+		if (!is_null($_newsletter)) {
+			$this->newsletter = $_newsletter;
+		}
+	}
+
 	public function getPhoto(){
 		return $this->photo;
+	}
+
+	public function setPhoto($_photo){
+		if (!is_null($_photo)) {
+			$this->photo = $_photo;
+		}
 	}
 
 	public function getAge(){
--- src/Models/AdherentSportif.php
+++ src/Models/AdherentSportif.php
@@ -49,48 +49,120 @@ class AdherentSportif
 		return $this->no_adherent;
 	}
 
+	public function setNoAdherent($_no_adherent){
+		if (!is_null($_no_adherent)) {
+			$this->no_adherent = $_no_adherent;
+		}
+	}
+
 	public function getMembreAd71(){
 		return $this->membre_ad71;
+	}
+
+	public function setMembreAd71($_membre_ad_71){
+		if (!is_null($_membre_ad_71)) {
+			$this->membre_ad71 = $_membre_ad_71;
+		}
 	}
 
 	public function getDojo(){
 		return $this->dojo;
 	}
 
+	public function setDojo($_dojo){
+		if (!is_null($_dojo)) {
+			$this->dojo = $_dojo;
+		}
+	}
+
 	public function getCertificatMed(){
 		return $this->certificat_med;
+	}
+
+	public function setCertificatMed($_certificat_med){
+		if (!is_null($_certificat_med)) {
+			$this->certificat_med = $_certificat_med;
+		}
 	}
 
 	public function getDateCertificat(){
 		return $this->date_certificat;
 	}
 
+	public function setDateCertificat($_date_certificat){
+		if (!is_null($_date_certificat)) {
+			$this->date_certificat = $_date_certificat;
+		}
+	}
+
 	public function getAttestationSante(){
 		return $this->attestation_sante;
+	}
+
+	public function setAttestationSante($_attestation_sante){
+		if (!is_null($_attestation_sante)) {
+			$this->attestation_sante = $_attestation_sante;
+		}
 	}
 
 	public function getAutorisationParent(){
 		return $this->autorisation_parent;
 	}
 
+	public function setAutorisationParent($_autorisation_parent){
+		if (!is_null($_autorisation_parent)) {
+			$this->autorisation_parent = $_autorisation_parent;
+		}
+	}
+
 	public function getAutorisationPrelevement(){
 		return $this->autorisation_prelevement;
+	}
+
+	public function setAutorisationPrelevement($_autorisation_prelevement){
+		if (!is_null($_autorisation_prelevement)) {
+			$this->autorisation_prelevement = $_autorisation_prelevement;
+		}
 	}
 
 	public function getLocationKimono(){
 		return $this->location_kimono;
 	}
 
+	public function setLocationKimono($_location_kimono){
+		if (!is_null($_location_kimono)) {
+			$this->location_kimono = $_location_kimono;
+		}
+	}
+
 	public function getNoPasseport(){
 		return $this->no_passeport;
+	}
+
+	public function setNoPasseport($_no_passeport){
+		if (!is_null($_no_passeport)) {
+			$this->no_passeport = $_no_passeport;
+		}
 	}
 
 	public function getCategorieAge(){
 		return $this->categorie_age;
 	}
 
+	public function setCategorieAge($_categorie_age){
+		if (!is_null($_categorie_age)) {
+			$this->categorie_age = $_categorie_age;
+		}
+	}
+
 	public function getCategoriePoids(){
 		return $this->categorie_poids;
+	}
+
+	public function setCategoriePoids($_categorie_poids){
+		if (!is_null($_categorie_poids)) {
+			$this->categorie_poids = $_categorie_poids;
+		}
 	}
 }
 ?>
